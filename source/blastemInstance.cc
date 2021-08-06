ace986c277118150d117e4e6195fef7bb12738e9
Sergio Martin <martiser@ethz.ch>
2021-08-06 21:28:48 +0200

    Updating lvl12

--- source/blastemInstance.cc
+++ source/blastemInstance.cc
@@ -313,6 +313,13 @@ void blastemInstance::printState()
  printf("[Jaffar2]  + Current RNG Value: 0x%X\n", _state.rngValue);
  printf("[Jaffar2]  + Game / Video Frame: %d / %d\n", _state.gameFrame, _state.videoFrame);
  printf("[Jaffar2]  + Checkpoint Pointer: 0x%X\n", _state.checkpointPointer);
+
+ if (_state.currentLevel == 12)
+ {
+  printf("[Jaffar2]  + Exit Door Status: %d\n", _state.lvl12ExitDoor);
+  printf("[Jaffar2]  + Weird Door Status: %d\n", _state.lvl12WeirdRoomDoor);
+ }
+
  printf("[Jaffar2]  + [Kid]   Room: %d, Pos.x: %3d, Pos.y: %3d (+%3d), Frame: %2d, Direction: %s, HP: %d/%d\n", _state.kidRoom, _state.kidPositionX, _state.kidPositionY, _state.kidFallingSpeed, _state.kidFrame, _state.kidDirection == 255 ? "L" : "R", _state.kidCurrentHP, _state.kidMaxHP);
  printf("[Jaffar2]  + [Guard] Room: %d, Pos.x: %3d, Pos.y: %3d, Frame: %3d, Direction: %s, HP: %d/%d\n", _state.guardRoom, _state.guardPositionX, _state.guardPositionY, _state.guardFrame, _state.guardDirection == 255 ? "L" : "R", _state.guardCurrentHP, _state.guardMaxHP);
 }
